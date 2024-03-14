80603c22c716847bae59ee796e33571e75d93b81
Tavo Cruz <madecruz2@gmail.com>
2024-03-14 15:59:19 -0700

    start logo header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Logo from './pages/logo/Logo';
+
+function App() {
+
+  return (
+    <div className=''>
+      <Logo />
+      {/* Pics */}
+      {/* Shorts */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/logo/Logo.jsx
@@ -0,0 +1,17 @@
+import SearchIcon from '@mui/icons-material/Search';
+
+const Logo = () => {
+  return (
+    <div className="bg-red-500 p-20 flex justify-center">
+      <div className="bg-green-800 flex justify-center gap-20 p-10 rounded-full w-1/2">
+        <img src='/vite.svg' alt='' />
+        <input className='bg-white rounded-lg px-4 py-2' type='text' placeholder='search'/>
+        <div className='bg-green-500 rounded-full flex items-center justify-center h-10 w-10'>
+          <SearchIcon />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Logo
